45e223adda96d0434ae622e947823d0eb6399a91
limjae <limjae96@gmail.com>
2022-07-20 23:09:58 +0900

    Update README.md

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 Study About JPA/Hibernate
 
 ## - [자바 ORM 표준 JPA 프로그래밍 강의](https://www.inflearn.com/course/ORM-JPA-Basic#curriculum)
+* 학습한 내용의 일부를 코드와 주석으로 정리했습니다
 
-### 1. entity/EntityGuide
-* 엔티티를 Mapping시 유의사항에 대하여 서술하였습니다.
+### entity/EntityGuide
+* 엔티티를 Mapping시 유의사항에 대하여 정리하였습니다.
+
+### repository/
+* entity manager를 통해 db에서 조회하고 저장하는 코드예시를 정리했습니다.
+
+### AppRunner, Runcase
+* 간단한 동작 테스트
--- src/main/java/practice/jpa_basic/AppRunner.java
+++ src/main/java/practice/jpa_basic/AppRunner.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-//CommandLineRunner 인터페이스를 구현하고 @Component annotaion을 사용하면 Compoent Scan 이후 구동시점에 run이 실행됩니다.
-//https://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/CommandLineRunner.html
 public class AppRunner implements CommandLineRunner {
     private final RunCase runCase;
 
